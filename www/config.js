b6caf9279baabf330adc7ac23e67bd326629b1c3
Chester A. Unal <chester.a.unal@arinc9.com>
2026-02-25 11:17:49 +0200

    Change to BSBF Firmware Selector
    Signed-off-by: Chester A. Unal <chester.a.unal@arinc9.com>

--- www/config.js
+++ www/config.js
@@ -4,24 +4,18 @@ var config = {
   // Show help text for images
   show_help: true,
 
-  // Versions list (optional if provided by .versions.json)
-  versions: ["23.05.4", "19.07.10"],
-
-  // Pre-selected version (optional if provided by .versions.json)
-  default_version: "23.05.4",
+  // Insert snapshot versions (optional)
+  show_snapshots: true,
 
   // Image download URL (e.g. "https://downloads.openwrt.org")
-  image_url: "../misc",
-
-  // Insert snapshot versions (optional)
-  //show_snapshots: true,
+  image_url: "https://downloads.openwrt.org",
 
   // Info link URL (optional)
   info_url: "https://openwrt.org/start?do=search&id=toh&q={title} @toh",
 
   // Attended Sysupgrade Server support (optional)
   asu_url: "https://sysupgrade.openwrt.org",
-  asu_extra_packages: ["luci", "luci-app-attendedsysupgrade"],
+  asu_extra_packages: ["luci", "luci-app-attendedsysupgrade", "kmod-ifb", "kmod-nft-tproxy", "bsbf-bonding"],
   // Additional repositories for ASU build requests (optional)
   // asu_repositories: {
   //   my_feed: "https://example.com/packages/{openwrt_branch}/{target}/{subtarget}",
@@ -30,4 +24,7 @@ var config = {
   // asu_repository_keys: [
   //   "-----BEGIN PUBLIC KEY-----\n...\n-----END PUBLIC KEY-----",
   // ],
+
+  /* bsbf-add-client API URL */
+  bsbf_add_client_url: "https://api.bondingshouldbefree.org/bsbf-add-client",
 };
